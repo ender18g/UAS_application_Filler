b5bb61f0b1e54e8a0bd5798a28043cb605c864e0
Allan Elsberry <allan.elsberry@gmail.com>
2020-05-26 13:51:50 -0400

    updated requirements

---
+++ sheets.py
@@ -0,0 +1,61 @@
+from __future__ import print_function
+from time import sleep
+import re
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+
+# The ID and range of a sample spreadsheet.
+sample_id = '1DIQeQAnnn6cUuRCi2i1XvBbAeJwdbsN4Ba2cbV1Gkgw'
+sample_range = 'Inputs!A:AO'
+
+def pull_sheet(ss_id,tab_range):
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=ss_id,
+                                range=tab_range).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+    else:
+        return values
+
+
+
+
+
+
+if __name__ == "__main__":
+  results = pull_sheet(sample_id,sample_range)
+  print(results)
